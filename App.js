fce26464658a9e542bae77568a59f3ee7717b045
SaintHazzard <chamitos1998@hotmail.com>
2024-03-06 15:23:33 -0500

    feat: :sparkles: Sidebar and elementBard
    Se configura el web component side-bar y element sidebar, donde element es el contenido de side-bar

--- App.js
+++ App.js
@@ -25,25 +25,44 @@ class device {
 
 
 
-let data = await duckFetch('InventarioCampuslands', 1, 'GET', null);
-let dataObject = {};
+// let data = await duckFetch('products', 1, 'GET', null);
+// let dataObject = {};
 
 
-(async () => {
-  function instanciarObjetos() {
-    for (const item of data) {
-      dataObject[item.id] = item;
+// (async () => {
+//   function instanciarObjetos() {
+//     for (const item of data) {
+//       dataObject[item.id] = item;
+//     }
+//     console.log(dataObject);
+//   }
+//   await instanciarObjetos(dataObject);
+// })();
+
+
+const navItems = document.querySelectorAll('[class="nav-item"]');
+
+navItems.forEach(li => { li.addEventListener('click', toggleCollapse) });
+console.log(navItems);
+
+function toggleCollapse(event) {
+  const navItem = event.target.closest('.nav-item');
+
+  if (navItem) {
+    const optionContent = navItem.nextElementSibling;
+
+    if (optionContent && optionContent.classList.contains('option-content')) {
+      optionContent.style.display = (optionContent.style.display === 'block' || optionContent.style.display === '') ? 'none' : 'block';
+
+      const option = document.querySelector('.option');
+      option.classList.toggle('expanded');
     }
-    console.log(dataObject);
   }
-  await instanciarObjetos(dataObject);
-})();
+}
 
-
-
-
-let arrayProveedores = [...new Set(data.map(item => item.Proveedor).filter(proveedor => proveedor !== undefined))];
-console.log(arrayProveedores);
+// let arrayProveedores = [...new Set(data.map(item => item.Proveedor).filter(proveedor => proveedor !== undefined))];
+// let arrayUbicaciones = [...new Set(data.map(item => item.Ubicacion).filter(Ubicacion => Ubicacion !== undefined))];
+// console.log(arrayUbicaciones);
 
 
 
