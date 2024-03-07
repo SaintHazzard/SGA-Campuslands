96147277b0e671da94910d2de3d437b6301bb817
SaintHazzard <chamitos1998@hotmail.com>
2024-03-06 23:05:02 -0500

    feat: :sparkles: modulo agregar activo
    Se termina el modulo crear activo, aun pendiente de revision

--- App/webComponents/agregarActivo.js
+++ App/webComponents/agregarActivo.js
@@ -1,11 +1,13 @@
-export class agregarActivo extends HTMLElement {
-    constructor(){
-        super();
-        this.render();
-    }
+import { duckFetch } from "../../Api/duckFetch.js";
 
-    render(){
-        this.innerHTML = /* html */ `
+export class agregarActivo extends HTMLElement {
+  constructor() {
+    super();
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = /* html */ `
         <div class="m-5" id="formAdd">
             <form class="row g-3 needs-validation" novalidate>
                 <div class="col-md-4">
@@ -24,16 +26,8 @@ export class agregarActivo extends HTMLElement {
                 </div>
                 <div class="col-md-4">
                     <label for="validationCustom04" class="form-label">Marca</label>
-                    <select class="form-select" id="validationCustom04" required>
+                    <select class="form-select" id="validationCustom03" required>
                       <option selected disabled value="">Seleccione...</option>
-                      <option>LG</option>
-                      <option>Compumax</option>
-                      <option>Logitech</option>
-                      <option>BENQ</option>
-                      <option>ASUS</option>
-                      <option>LENOVO</option>
-                      <option>HP</option>
-                      <option>Otro...</option>
                     </select>
                     <div class="invalid-feedback">
                       Please select a valid state.
@@ -41,12 +35,8 @@ export class agregarActivo extends HTMLElement {
                 </div>
                 <div class="col-md-3">
                     <label for="validationCustom04" class="form-label">Categoria</label>
-                    <select class="form-select" id="validationCustom04" required>
+                    <select class="form-select" id="validationCustom05" required>
                       <option selected disabled value="">Seleccione...</option>
-                      <option>Equipo de computo</option>
-                      <option>Electrodomestico</option>
-                      <option>Juego</option>
-                      <option>Otro...</option>
                     </select>
                     <div class="invalid-feedback">
                       Please select a valid state.
@@ -54,16 +44,8 @@ export class agregarActivo extends HTMLElement {
                 </div>
                 <div class="col-md-3">
                     <label for="validationCustom04" class="form-label">Tipo</label>
-                    <select class="form-select" id="validationCustom04" required>
+                    <select class="form-select" id="validationCustom06" required>
                       <option selected disabled value="">Seleccione...</option>
-                      <option>Monitor</option>
-                      <option>CPU</option>
-                      <option>Teclado</option>
-                      <option>Mouse</option>
-                      <option>Aire Acondicionado</option>
-                      <option>Portatil</option>
-                      <option>Impresora</option>
-                      <option>Otro...</option>
                     </select>
                     <div class="invalid-feedback">
                       Please select a valid state.
@@ -71,16 +53,16 @@ export class agregarActivo extends HTMLElement {
                 </div>
                 <div class="col-md-3">
                   <label for="validationCustom03" class="form-label">Valor Unitario</label>
-                  <input type="text" class="form-control" id="validationCustom03" required>
+                  <input type="text" class="form-control" id="validationCustom07" required>
                   <div class="invalid-feedback">
                     Please provide a valid value.
                   </div>
                 </div>
                 <div class="col-md-3">
                   <label for="validationCustom04" class="form-label">Proveedor</label>
-                  <select class="form-select" id="validationCustom04" required>
-                    <option selected disabled value="">Choose...</option>
-                    <option>...</option>
+                  <select class="form-select" id="validationCustom08" required>
+                    <option selected disabled value="">Seleccione...</option>
+                  
                   </select>
                   <div class="invalid-feedback">
                     Please select a valid state.
@@ -88,26 +70,22 @@ export class agregarActivo extends HTMLElement {
                 </div>
                 <div class="col-md-4">
                     <label for="validationCustom03" class="form-label">Número de Serial</label>
-                    <input type="text" class="form-control" id="validationCustom03" required>
+                    <input type="text" class="form-control" id="validationCustom09" required>
                     <div class="invalid-feedback">
                       Please provide a valid serial.
                     </div>
                 </div>
                 <div class="col-md-4">
                     <label for="validationCustom03" class="form-label">Empresa responsable</label>
-                    <input type="text" class="form-control" id="validationCustom03" required>
+                    <input type="text" class="form-control" id="validationCustom10" required>
                     <div class="invalid-feedback">
                       Please provide a valid serial.
                     </div>
                 </div>
                 <div class="col-md-3">
                     <label for="validationCustom04" class="form-label">Estado</label>
-                    <select class="form-select" id="validationCustom04" required>
+                    <select class="form-select" id="validationCustom11" required>
                       <option selected disabled value="">Seleccione...</option>
-                      <option>No asignado</option>
-                      <option>Asignado</option>
-                      <option>Dado de baja por daño</option>
-                      <option>En reparación y/oGarantia</option>
                     </select>
                     <div class="invalid-feedback">
                       Please select a valid state.
@@ -119,7 +97,48 @@ export class agregarActivo extends HTMLElement {
               </form>
         </div>
         `
-    }
+  }
+  setupValidation() {
+    const forms = this.querySelectorAll('.needs-validation');
+    forms.forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      });
+    });
+  }
+  connectedCallback() {
+    this.setupValidation()
+    this.addOptions();
+  }
+  addOptions() {
+    const selectElements = this.querySelectorAll('.form-select')
+    duckFetch('marcas', 1, 'GET', null).then(data => {
+      const marcas = data.map(marca => marca.nombre);
+      selectElements[0].innerHTML += marcas.map(marca => `<option>${marca}</option>`).join('');
+    });
+    duckFetch('categories', 1, 'GET', null).then(data => {
+      const categorias = data.map(categoria => categoria.nombre);
+      selectElements[1].innerHTML += categorias.map(categoria => `<option>${categoria}</option>`).join('');
+    });
+    duckFetch('tipos', 1, 'GET', null).then(data => {
+      const tipos = data.map(tipo => tipo.nombre);
+      selectElements[2].innerHTML += tipos.map(tipo => `<option>${tipo}</option>`).join('');
+    });
+    duckFetch('providers', 1, 'GET', null).then(data => {
+      const proveedores = data.map(proveedor => proveedor.nombre);
+      selectElements[3].innerHTML += proveedores.map(proveedor => `<option>${proveedor}</option>`).join('');
+    });
+    duckFetch('estados', 1, 'GET', null).then(data => {
+      const estados = data.map(estado => estado.nombre);
+      selectElements[4].innerHTML += estados.map(estado => `<option>${estado}</option>`).join('');
+    });
+  }
 }
+
 
 customElements.define('agregar-activo', agregarActivo)
