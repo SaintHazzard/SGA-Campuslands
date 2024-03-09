adcfa9602530e7312411bede5ceb5fa3d09a9f30
SaintHazzard <chamitos1998@hotmail.com>
2024-03-09 17:35:33 -0500

    import duckFetch to autoIncremental.js

--- Api/autoIncremental.js
+++ Api/autoIncremental.js
@@ -1,3 +1,4 @@
+import { duckFetch } from './duckFetch.js';
 async function autoIncrementalId() {
   let dataId = await duckFetch('products', null, 'GET', null);
   let newId = Math.max(...dataId.map(product => Number(product.id))) + 1;
