ae5dc4f26b0681d8bafc83336e3c6811b73fab5d
SaintHazzard <chamitos1998@hotmail.com>
2024-03-10 16:06:19 -0500

    retornar asignacion

---
+++ App/webComponents/retornarAsignacion.js
@@ -0,0 +1,37 @@
+export class retornarAsignacion extends HTMLElement {
+    constructor() {
+      super();
+      this.render();
+    }
+    render() {
+      this.innerHTML = /* html */ `
+        <style>
+          /* Estilos aquí si es necesario */
+        </style>
+        <p class="mx-5"><strong>Retornar Asignacion</strong></p>
+        <div class="m-5" id="formAdd">
+          <form class="row g-3 needs-validation" novalidate>
+            <div class="col-md-3">
+                      <label for="validationCustom04" class="form-label">Buscar Activo</label>
+                      <select class="form-select" id="validationCustom01" required>
+                        <option selected disabled value="">Seleccione...</option>
+                      </select>
+                      <div class="invalid-feedback">
+                        Seleccione una persona.
+                      </div>
+                  </div>
+            <div class="col-12">
+                      <button class="btn btn-danger" type="submit" id="addSomething">Retornar</button>
+            </div>
+          </form>
+        </div>
+      `;
+      this.chargeData()
+    }
+  
+    async chargeData() {
+      //
+    }
+  }
+  
+  customElements.define('retornaractivo-asignacione', retornarAsignacion)
