e4ae6dc87184e2c4a50cb70a4b160c9fce17f03d
SaintHazzard <chamitos1998@hotmail.com>
2024-03-10 13:43:07 -0500

    fix bug

---
+++ App/webComponents/asignarActivos.js
@@ -0,0 +1,34 @@
+export class asignarActivo extends HTMLElement {
+    constructor() {
+      super();
+      this.render();
+    }
+  
+    render() {
+      this.innerHTML = /* html */ `
+      <buscar-activo></buscar-activo>
+      <div class="container mt-3">
+        <h2>Asignaciones Creadas</h2>
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Asignaciones</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Asignacion1</td>
+            </tr>
+            </tbody>
+        </table>
+      </div>
+    
+
+      `;
+    }
+  }
+  
+  
+  customElements.define('asignar-activo', asignarActivo)
--- App/webComponents/elementAsignacion.js
+++ App/webComponents/elementAsignacion.js
@@ -7,12 +7,21 @@ export class elementAsignacion extends HTMLElement {
     render() {
       this.innerHTML = /* html */ `<ul class="nav nav-pills flex-column mb-auto">
           <li>
-            <a href="" class="nav-link text-white" aria-current="page">
+            <a href="#" class="nav-link text-white" aria-current="page">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M20 4H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zM8.715 8c1.151 0 2 .849 2 2s-.849 2-2 2-2-.849-2-2 .848-2 2-2zm3.715 8H5v-.465c0-1.373 1.676-2.785 3.715-2.785s3.715 1.412 3.715 2.785V16zM19 15h-4v-2h4v2zm0-4h-5V9h5v2z"></path></svg>
             Crear Asignacion            
             </a>
           </li>
-          
+          <li>
+            <a href="#" class="nav-link text-white" aria-current="page">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M4.5 8.552c0 1.995 1.505 3.5 3.5 3.5s3.5-1.505 3.5-3.5-1.505-3.5-3.5-3.5-3.5 1.505-3.5 3.5zM19 8h-2v3h-3v2h3v3h2v-3h3v-2h-3zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2z"></path></svg>            Asignar activos            
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-white" aria-current="page">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M19.89 10.105a8.696 8.696 0 0 0-.789-1.456l-1.658 1.119a6.606 6.606 0 0 1 .987 2.345 6.659 6.659 0 0 1 0 2.648 6.495 6.495 0 0 1-.384 1.231 6.404 6.404 0 0 1-.603 1.112 6.654 6.654 0 0 1-1.776 1.775 6.606 6.606 0 0 1-2.343.987 6.734 6.734 0 0 1-2.646 0 6.55 6.55 0 0 1-3.317-1.788 6.605 6.605 0 0 1-1.408-2.088 6.613 6.613 0 0 1-.382-1.23 6.627 6.627 0 0 1 .382-3.877A6.551 6.551 0 0 1 7.36 8.797 6.628 6.628 0 0 1 9.446 7.39c.395-.167.81-.296 1.23-.382.107-.022.216-.032.324-.049V10l5-4-5-4v2.938a8.805 8.805 0 0 0-.725.111 8.512 8.512 0 0 0-3.063 1.29A8.566 8.566 0 0 0 4.11 16.77a8.535 8.535 0 0 0 1.835 2.724 8.614 8.614 0 0 0 2.721 1.833 8.55 8.55 0 0 0 5.061.499 8.576 8.576 0 0 0 6.162-5.056c.22-.52.389-1.061.5-1.608a8.643 8.643 0 0 0 0-3.45 8.684 8.684 0 0 0-.499-1.607z"></path></svg>            Retornar Activo           
+            </a>
+          </li>
         </ul>`;
     }
   }
