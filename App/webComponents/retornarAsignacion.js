b158e832c852466a4c6d8b4039ab2bd726dc0236
lumigamher <lumigam@hotmail.com>
2024-03-11 15:16:46 -0500

    fix(Web Componnets): :bug: correccion de elementos

--- App/webComponents/retornarAsignacion.js
+++ App/webComponents/retornarAsignacion.js
@@ -12,16 +12,16 @@ export class retornarAsignacion extends HTMLElement {
         <div class="m-5" id="formAdd">
           <form class="row g-3 needs-validation" novalidate>
             <div class="col-md-3">
-                      <label for="validationCustom04" class="form-label">Buscar Activo</label>
-                      <select class="form-select" id="validationCustom01" required>
-                        <option selected disabled value="">Seleccione...</option>
-                      </select>
-                      <div class="invalid-feedback">
-                        Seleccione una persona.
-                      </div>
-                  </div>
+                <label for="validationCustom04" class="form-label">Buscar Activo</label>
+                <select class="form-select" id="validationCustom01" required>
+                  <option selected disabled value="">Seleccione...</option>
+                </select>
+                <div class="invalid-feedback">
+                  Seleccione una persona.
+                </div>
+            </div>
             <div class="col-12">
-                      <button class="btn btn-danger" type="submit" id="addSomething">Retornar</button>
+                <button class="btn btn-danger" type="submit" id="addSomething">Retornar</button>
             </div>
           </form>
         </div>
